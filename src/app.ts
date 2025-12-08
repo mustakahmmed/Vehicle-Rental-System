781e98868396b693f0875c164c3a5f0fad21d1df
mustak <mustakahmmed88@gmail.com>
2025-12-08 10:57:22 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+const app = express()
+import { Pool } from "pg";
+import config from "./config";
+
+
+const pool = new Pool({connectionString:`${config.connection_str}`})
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+export default app;
